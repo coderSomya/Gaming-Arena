13f76fa2fadb2856b6b66947f546f8eed11e2426
Somyajeet <isomya13@gmail.com>
2024-07-09 21:53:02 +0530

    updated index.js to be managed by manager

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
-import Player from './player.js';
-import Game from './game.js';
+import Player from "./player.js";
+import GameManager from "./gameManager.js";
+
+const gameManager = new GameManager();
 
 const playerA = new Player('Player A', 50, 5, 10);
 const playerB = new Player('Player B', 100, 10, 5);
+const playerC = new Player('Player C', 80, 7, 8);
 
-const game = new Game(playerA, playerB);
-game.start();
+gameManager.enterArena(playerA);
+gameManager.enterArena(playerB);
+gameManager.enterArena(playerC);
+
+// This simulates more players entering the arena at a later time
+setTimeout(() => {
+  const playerD = new Player('Player D', 70, 6, 9);
+  gameManager.enterArena(playerD);
+}, 5000);
