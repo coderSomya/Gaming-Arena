a848547d91db6b69e38a785782239a8dc34d009b
Somyajeet <isomya13@gmail.com>
2024-07-09 19:56:01 +0530

    added logging

--- src/game.js
+++ src/game.js
@@ -1,11 +1,38 @@
+import fs from 'fs';
+import path from 'path';
+
+import { fileURLToPath } from 'url';
+
+// Define __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 class Game {
     constructor(playerA, playerB) {
       this.playerA = playerA;
       this.playerB = playerB;
+      this.id = Date.now();
+      this.logFile = this.createLogFile();
     }
+
+    createLogFile() {
+        const recordsDir = path.join(__dirname, '../records');
+        if (!fs.existsSync(recordsDir)) {
+          fs.mkdirSync(recordsDir, { recursive: true });
+        }
+        const logFileName = `game_${this.id}.log`;
+        const logFilePath = path.join(recordsDir, logFileName);
+        fs.writeFileSync(logFilePath, 'Game Log\n');
+        return logFilePath;
+      }
+    
+      logToFile(message) {
+        fs.appendFileSync(this.logFile, `${message}\n`);
+      }
   
     start() {
-      console.log(`Game started between ${this.playerA.getName()} and ${this.playerB.getName()}`);
+      console.log("game started");
+      this.logToFile(`Game started between ${this.playerA.getName()} and ${this.playerB.getName()}`);
       let attacker = this.playerA.getHealth() <= this.playerB.getHealth() ? this.playerA : this.playerB;
       let defender = attacker === this.playerA ? this.playerB : this.playerA;
   
@@ -15,8 +42,9 @@ class Game {
         // Swap attacker and defender
         [attacker, defender] = [defender, attacker];
       }
-  
-      console.log(`Game Over! Winner: ${this.playerA.isAlive() ? this.playerA.getName() : this.playerB.getName()}`);
+
+      this.logToFile(`Game Over! Winner: ${this.playerA.isAlive() ? this.playerA.getName() : this.playerB.getName()}`);
+      console.log("game ended..winner: ",this.playerA.isAlive() ? this.playerA.getName() : this.playerB.getName())
     }
   
     executeTurn(attacker, defender) {
@@ -26,11 +54,11 @@ class Game {
       const damageDealt = attacker.getAttack() * attackRoll - defender.getStrength() * defenseRoll;
       if (damageDealt > 0) {
         defender.setHealth(defender.getHealth() - damageDealt);
-        console.log(`${attacker.getName()} deals ${damageDealt} damage to ${defender.getName()}`);
-        console.log(`${defender.getName()} health is now ${defender.getHealth()}`);
+        this.logToFile(`${attacker.getName()} deals ${damageDealt} damage to ${defender.getName()}`);
+        this.logToFile(`${defender.getName()} health is now ${defender.getHealth()}`);    
       } else {
-        console.log(`${defender.getName()} successfully defended against ${attacker.getName()}`);
-      }
+        this.logToFile(`${defender.getName()} successfully defended against ${attacker.getName()}`);
+    }
     }
   }
   
