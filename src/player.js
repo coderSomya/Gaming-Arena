46e36c245d5c3312796dd26731c4be381a75de32
Somyajeet <isomya13@gmail.com>
2024-07-09 18:45:44 +0530

    added player class

---
+++ src/player.js
@@ -0,0 +1,48 @@
+class Player{
+
+    #name;
+    #health;
+    #strength;
+    #attack;
+
+    //setting attributes as private, for encapusulation
+
+    constructor(name="player", health=0, attack=0, strength=0){
+        //initialize attributes
+        this.#name = name;
+        this.#health = health;
+        this.#attack = attack;
+        this.#strength = strength;
+    }
+
+    //define getters and setter
+    getName() {
+        return this.#name;
+    }
+    
+    getHealth() {
+        return this.#health;
+    }
+
+    setHealth(health) {
+        this.#health = health;
+    }
+
+    getAttack() {
+        return this.#attack;
+    }
+
+    getStrength() {
+        return this.#strength;
+    }
+
+    isAlive() {
+        return this.#health > 0;
+    }
+    
+    rollDice() {
+        return Math.floor(Math.random() * 6) + 1;
+    }
+}
+
+module.exports = Player;
